943d8925669eb0da53dd7c243798f8c3435e36e3
spewarren <sw15256@my.bristol.ac.uk>
2017-12-11 16:38:35 +0000

    Revert "Feature/ray animation"

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
-using System.Collections;
 
 public class PlayerShoot : NetworkBehaviour {
 
@@ -15,51 +14,22 @@ public class PlayerShoot : NetworkBehaviour {
     [SerializeField]
     private LayerMask mask;
 
-	private AudioSource shootAudio;
-	private LineRenderer laserLine;
-	private float nextFire;
-	private WaitForSeconds shotDuration = new WaitForSeconds(0.05f);
-
-	void Start () {
-		laserLine = GetComponent<LineRenderer> ();
-		shootAudio = GetComponent<AudioSource> ();
-	}
-
 	void Update () {
-		if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+		if (Input.GetButtonDown("Fire1"))
         {
-			nextFire = Time.time + bolt.fireRate;
             Shoot();
         }
-	}
-
-	IEnumerator Wait() {
-		shootAudio.Play ();
-		laserLine.enabled = true;
-		yield return shotDuration;
-		laserLine.enabled = false;
-
 	}
 
     [Client]
     void Shoot()
     {
-
-		laserLine.SetPosition(0, crossbow.transform.position);
-
-		StartCoroutine (Wait ());
-
         RaycastHit hit;
-		if (Physics.Raycast (crossbow.transform.position, crossbow.transform.forward, out hit, bolt.range, mask)) {
-			CmdPlayerShot (hit.collider.name, bolt.damage/*, hit.normal*/);
-			laserLine.SetPosition (1, hit.point);
-		} else {
-			//maybe extract this too
-			laserLine.SetPosition (1, crossbow.transform.position + crossbow.transform.forward * bolt.range);
-		}
-
-		//MOVE THIS THE FUCK OUT
-
+        if (Physics.Raycast(crossbow.transform.position, crossbow.transform.forward, out hit, bolt.range, mask))
+        {
+            CmdPlayerShot(hit.collider.name, bolt.damage);
+			Debug.DrawRay(crossbow.transform.position, crossbow.transform.forward, Color.green);
+        }
     }
 
     [Command]
@@ -70,10 +40,5 @@ public class PlayerShoot : NetworkBehaviour {
 		if (target.GetComponent<NPCHealth> ()) {
 			target.GetComponent<NPCHealth>().DeductHealth(damage);
 		}
-		if (target.transform != null /*&& target.collider.tag == "NPC"*/) {
-			target.transform.position = (target.transform.position /*- (normal of the hit)*/);
-		}
-
-
     }
 }
--- Assets/Scripts/Weapons/Bolt.cs
+++ Assets/Scripts/Weapons/Bolt.cs
@@ -5,6 +5,4 @@ public class Bolt {
     public string name = "StandardBolt";
     public float damage = 10f;
     public float range = 100f;
-	public float fireRate = 10.5f;
-
 }
