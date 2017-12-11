614c5d7172b14ac4ae5cd12aa105432cb19f7472
Spencer Warren <sw15256@my.bristol.ac.uk>
2017-12-11 16:22:35 +0000

    animations going good, need to get the transform working tho

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using System.Collections;
 
 public class PlayerShoot : NetworkBehaviour {
 
@@ -14,22 +15,51 @@ public class PlayerShoot : NetworkBehaviour {
     [SerializeField]
     private LayerMask mask;
 
+	private AudioSource shootAudio;
+	private LineRenderer laserLine;
+	private float nextFire;
+	private WaitForSeconds shotDuration = new WaitForSeconds(0.05f);
+
+	void Start () {
+		laserLine = GetComponent<LineRenderer> ();
+		shootAudio = GetComponent<AudioSource> ();
+	}
+
 	void Update () {
-		if (Input.GetButtonDown("Fire1"))
+		if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
         {
+			nextFire = Time.time + bolt.fireRate;
             Shoot();
         }
+	}
+
+	IEnumerator Wait() {
+		shootAudio.Play ();
+		laserLine.enabled = true;
+		yield return shotDuration;
+		laserLine.enabled = false;
+
 	}
 
     [Client]
     void Shoot()
     {
+
+		laserLine.SetPosition(0, crossbow.transform.position);
+
+		StartCoroutine (Wait ());
+
         RaycastHit hit;
-        if (Physics.Raycast(crossbow.transform.position, crossbow.transform.forward, out hit, bolt.range, mask))
-        {
-            CmdPlayerShot(hit.collider.name, bolt.damage);
-			Debug.DrawRay(crossbow.transform.position, crossbow.transform.forward, Color.green);
-        }
+		if (Physics.Raycast (crossbow.transform.position, crossbow.transform.forward, out hit, bolt.range, mask)) {
+			CmdPlayerShot (hit.collider.name, bolt.damage, hit.normal);
+			laserLine.SetPosition (1, hit.point);
+		} else {
+			//maybe extract this too
+			laserLine.SetPosition (1, crossbow.transform.position + crossbow.transform.forward * bolt.range);
+		}
+
+		//MOVE THIS THE FUCK OUT
+
     }
 
     [Command]
@@ -40,5 +70,10 @@ public class PlayerShoot : NetworkBehaviour {
 		if (target.GetComponent<NPCHealth> ()) {
 			target.GetComponent<NPCHealth>().DeductHealth(damage);
 		}
+		if (target.transform != null && target.collider.tag == "NPC") {
+			target.transform.position = (target.transform.position -((2) * 1/*normal of the hit*/));
+		}
+
+
     }
 }
