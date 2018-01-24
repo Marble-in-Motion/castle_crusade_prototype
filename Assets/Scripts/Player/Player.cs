3b1f319b8c54c51234d42ad575dbf0357a7f8c2d
Gareth Carless <gc15927@my.bristol.ac.uk>
2018-01-24 15:54:28 +0000

    setup five correct spawn locations, enabled terrain navmesh

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -89,16 +89,26 @@ public class Player : NetworkSetup
                 int currency = teamController.SpendGold(10);
                 this.GetComponentInChildren<Text>().text = "Coin: " + currency.ToString();
             }
-            else if (Input.GetKeyDown(KeyCode.Q))
+            else if (Input.GetKeyDown(KeyCode.Y))
             {
-                Debug.Log("Q_1: " + id);
                 CmdRequestOffensiveTroopSpawn(0, 0);
             }
-            else if (Input.GetKeyDown(KeyCode.W))
+            else if (Input.GetKeyDown(KeyCode.J))
             {
-                Debug.Log("W_2: " + id);
                 CmdRequestOffensiveTroopSpawn(0, 1);
             }
+			else if (Input.GetKeyDown(KeyCode.N))
+			{
+				CmdRequestOffensiveTroopSpawn(0, 2);
+			}
+			else if (Input.GetKeyDown(KeyCode.B))
+			{
+				CmdRequestOffensiveTroopSpawn(0, 3);
+			}
+			else if (Input.GetKeyDown(KeyCode.G))
+			{
+				CmdRequestOffensiveTroopSpawn(0, 4);
+			}
         }
     }
 
