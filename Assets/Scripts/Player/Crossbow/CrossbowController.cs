2d6e9c0e6a31af3b2e09ac7f6a08841ad9efe8c4
Gareth Carless <gc15927@my.bristol.ac.uk>
2018-02-05 15:40:44 +0000

    fixed shooting

--- Assets/Scripts/Player/Crossbow/CrossbowController.cs
+++ Assets/Scripts/Player/Crossbow/CrossbowController.cs
@@ -2,77 +2,34 @@
 using UnityEngine.Networking;
 using System.Collections;
 
-public class CrossbowController : NetworkBehaviour {
+public class CrossbowController : MonoBehaviour {
 
-	public Bolt bolt;
+//    [SerializeField]
+//    private float lookSensitivity = 3f;
 
-    [SerializeField]
-    private float lookSensitivity = 3f;
+	private LineRenderer laserLine;
 
     CrossbowMotor motor;
 
-    [SerializeField]
-	private LayerMask mask;
-
 	private AudioSource shootAudio;
-	private LineRenderer laserLine;
 	private WaitForSeconds shotDuration = new WaitForSeconds(0.05f);
 
 	void Start () {
         motor = GetComponent<CrossbowMotor>();
 		laserLine = GetComponent<LineRenderer>();
 		shootAudio = GetComponent<AudioSource>();
-		bolt = new Bolt ();
+		laserLine.enabled = true;
 	}
 
-	void Update () {
-        UpdateMovement();
-		if (Input.GetButtonDown("Fire1"))
-		{
-			Debug.Log ("Shot fired");
-			Shoot();
-		}
-	}
-
-	IEnumerator Wait() {
+	public IEnumerator HandleShoot() {
 		shootAudio.Play();
 		laserLine.enabled = true;
 		yield return shotDuration;
 		laserLine.enabled = false;
 	}
 
-	[Client]
-	void Shoot()
-	{
-		laserLine.SetPosition(0, transform.position);
-		StartCoroutine(Wait());
-		RaycastHit hit;
-		if (Physics.Raycast(transform.position, transform.forward, out hit, bolt.range, mask)) {
-			Debug.Log("4");
-			CmdPlayerShot(hit.collider.name, bolt.damage);
-			laserLine.SetPosition(1, hit.point);
-		} else {
-			//calculate max range of projectile
-			Debug.Log("5");
-			laserLine.SetPosition(1, transform.position + transform.forward * bolt.range);
-		}
-	}
-
-	[Command]
-	public void CmdPlayerShot(string id, float damage)
-	{
-		Debug.Log(id + " hit");
-		GameObject target = GameObject.Find(id);
-		if (target.GetComponent<NPCHealth> ()) {
-			target.GetComponent<NPCHealth>().DeductHealth(damage);
-            if (!target.GetComponent<NPCHealth>().IsAlive())
-            {
-                this.GetComponentInParent<Player>().CmdAddGold(10);
-            }
-		}
-		if (target.transform != null /*&& target.collider.tag == "NPC"*/) {
-			target.transform.position = (target.transform.position /*- (normal of the hit)*/);
-		}
+	void Update () {
+        UpdateMovement();
 	}
 
     private void UpdateMovement()
