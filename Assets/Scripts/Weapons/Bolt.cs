f8c06ce8e7a5ad4969ba8e9c43d0c7b78e8862f4
GeorgeBall <gb15351@my.bristol.ac.uk>
2018-02-06 15:22:40 +0000

    bolt range and dmg

--- Assets/Scripts/Weapons/Bolt.cs
+++ Assets/Scripts/Weapons/Bolt.cs
@@ -3,6 +3,6 @@
 public class Bolt {
 
     public string name = "StandardBolt";
-    public float damage = 50f;
-    public float range = 100f;
+    public float damage = 100f;
+    public float range = 400f;
 }
