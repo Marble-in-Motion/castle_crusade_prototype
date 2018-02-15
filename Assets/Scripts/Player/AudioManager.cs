7e41f1cf2c3886eb8d1a9338ffb6c903dc5656cc
Gareth Carless <gc15927@my.bristol.ac.uk>
2018-02-15 17:42:12 +0000

    this commit message is for brad

---
+++ Assets/Scripts/Player/AudioManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	[SerializeField]
+	private AudioSource klaxon;
+
+	public void PlaySound(string audioSourceName) {
+		AudioSource audioSource = klaxon;
+		audioSource.Play ();
+	}
+
+}
