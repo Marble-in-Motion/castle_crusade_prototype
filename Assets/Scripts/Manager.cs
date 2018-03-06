81af09287d6a83feb3b534f7efce2c51c451d3e7
Gareth Carless <gc15927@my.bristol.ac.uk>
2018-03-06 15:43:53 +0000

    network hud now disappears when player joins

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Manager : NetworkManager
+{
+	public override void OnServerConnect(NetworkConnection conn)
+	{
+		Debug.Log("Server connected");
+	}
+
+	public override void OnClientConnect(NetworkConnection conn)
+	{
+		Debug.Log ("Client connected");
+		this.GetComponentInParent<NetworkManagerHUD> ().showGUI = false;
+	}
+
+	public override void OnServerReady(NetworkConnection conn)
+	{
+		Debug.Log ("Ready");
+		this.GetComponentInParent<NetworkManagerHUD> ().showGUI = false;
+	}
+}
