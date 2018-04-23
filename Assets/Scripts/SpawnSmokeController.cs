13fc8093e22566f3bb5e924f92c2d8b6091e665a
Gareth Carless <gc15927@my.bristol.ac.uk>
2018-04-23 09:43:50 +0100

    Feature/spawn smoke (#198)
    * forgot to add and commit everything
    
    * lowered spawn smoke

---
+++ Assets/Scripts/SpawnSmokeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSmokeController : MonoBehaviour {
+
+	private float destroyTime;
+
+	// Use this for initialization
+	void Start () {
+		destroyTime = Time.time + 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > destroyTime)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
