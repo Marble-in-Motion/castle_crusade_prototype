1650090dc1354e36e4195d81cd70c8e1e61da22c
AdamStein593 <ams593@hotmail.co.uk>
2018-04-24 13:10:23 +0100

    Feature/min danger capture (#203)
    * completed
    
    * changed min score to 1

--- Assets/Scripts/Params.cs
+++ Assets/Scripts/Params.cs
@@ -31,7 +31,7 @@ public class Params {
     public static float TROOP_SEND_DELAY_PER_TROOP = 0.5f;
     public static string IMAGE_INFERENCE_SCRIPT_PATH = @"C:\Users\SP\Documents\WORK\GP\tensorflow\tensorflow-for-poets-2\scripts\label_image_spesh.py";
     public static float TRAIN_SCREENSHOT_DELAY = 2;
-    public static int MIN_DANGER_SCORE_SCREENSHOT = 0; 
+    public static int MIN_DANGER_SCORE_SCREENSHOT = 1; 
 
 
     internal static float SEND_TROOP_ALERT_DELAY = 100;//10f;
