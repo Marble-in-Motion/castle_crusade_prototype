2c62cf1ceabd4215d2f8c7d5a8da5b51c777ca7e
Spencer Warren <spewarren@gmail.com>
2018-04-24 13:07:43 +0100

    completed

--- Assets/Scripts/HiResScreenShot.cs
+++ Assets/Scripts/HiResScreenShot.cs
@@ -35,22 +35,25 @@ public class HiResScreenShot : NetworkBehaviour
     {
         for (int i = 0; i < players.Length; i++)
         {
-            int playerId = players[i].GetComponent<Player>().GetId();
-            Camera camera = players[i].GetComponentInChildren<Camera>();
-            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
-            camera.targetTexture = rt;
-            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
-            camera.Render();
-            RenderTexture.active = rt;
-            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
-            camera.targetTexture = null;
-            RenderTexture.active = null; // JC: added to avoid errors
-            Destroy(rt);
-            byte[] bytes = screenShot.EncodeToPNG();
-            string filename = ScreenShotNameTrainData(resWidth, resHeight, playerId, dangers[i]);
+            if (dangers[i] >= Params.MIN_DANGER_SCORE_SCREENSHOT)
+            {
+                int playerId = players[i].GetComponent<Player>().GetId();
+                Camera camera = players[i].GetComponentInChildren<Camera>();
+                RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+                camera.targetTexture = rt;
+                Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+                camera.Render();
+                RenderTexture.active = rt;
+                screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+                camera.targetTexture = null;
+                RenderTexture.active = null; // JC: added to avoid errors
+                Destroy(rt);
+                byte[] bytes = screenShot.EncodeToPNG();
+                string filename = ScreenShotNameTrainData(resWidth, resHeight, playerId, dangers[i]);
 
-            Thread takeScreenShots = new Thread(() => ScreenShotThread(filename, bytes));
-            takeScreenShots.Start();
+                Thread takeScreenShots = new Thread(() => ScreenShotThread(filename, bytes));
+                takeScreenShots.Start();
+            }
         }
     }
 
